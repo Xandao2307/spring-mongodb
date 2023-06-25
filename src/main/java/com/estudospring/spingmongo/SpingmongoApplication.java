16163e01c007d57a297515ea64e2658690208d58
Alexandre Souza <allexandre.souza2307@gmail.com>
2023-06-25 18:18:04 -0300

    first commit

---
+++ src/main/java/com/estudospring/spingmongo/SpingmongoApplication.java
@@ -0,0 +1,13 @@
+package com.estudospring.spingmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingmongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingmongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estudospring/spingmongo/SpingmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudospring.spingmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingmongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

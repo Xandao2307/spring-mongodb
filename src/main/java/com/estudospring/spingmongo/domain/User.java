b69e18d7c1cae9345dd88e5e755e5075da27511a
Alexandre Souza <allexandre.souza2307@gmail.com>
2023-07-06 03:14:54 -0300

    referenciando os posts aos usuarios

--- src/main/java/com/estudospring/spingmongo/domain/User.java
+++ src/main/java/com/estudospring/spingmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.estudospring.spingmongo.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -13,6 +16,8 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
     public User() {
     }
 
@@ -46,6 +51,9 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
